587a64cc0d2f2a2f37c92487acfa147290f09153
Chris Bodenlos <hello_git@icloud.com>
2017-11-07 13:24:38 -0500

    Sanitize data from event calendar

--- views/widget.php
+++ views/widget.php
@@ -4,14 +4,14 @@
     <h2>Upcoming Events at the Guggenheim</h2>
     <?php
     foreach ( $events->instances as $current_event ) {
-        $event_date = $current_event->start_date . ' ' . $current_event->start_time;
+        $event_date = sanitize_text_field( $current_event->start_date ) . ' ' . sanitize_text_field($current_event->start_time );
         $event_date = DateTime::createFromFormat( 'Y-m-d H:i:s', $event_date )->format('l F j, Y \a\t g:ia');
         echo '<div>
-                <h3>' . $current_event->titles->en . '</h3>
+                <h3>' . sanitize_text_field( $current_event->titles->en ) . '</h3>
                 <span>' . $event_date . '</span>
-                <img src="' . $current_event->media[0]->assets->thumbnail->_links->_self->href . '">
-                <p>' . $this->word_count( strip_tags($current_event->descriptions->en), 30 ) . '</p>
-                <a href="' . $current_event->_links->web->href . '">More Information</a> >
+                <img src="' . esc_url ($current_event->media[0]->assets->thumbnail->_links->_self->href ) . '">
+                <p>' . $this->word_count( sanitize_text_field($current_event->descriptions->en), 30 ) . '</p>
+                <a href="' . esc_url( $current_event->_links->web->href ) . '">More Information</a> >
               </div>';
     }
     ?>
